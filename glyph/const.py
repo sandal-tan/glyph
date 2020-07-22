94f98d9b6b124ca91b6cfa1ce98203d57cc8f29b
Ian Baldwin <ian@iantbaldw.in>
2020-07-22 15:23:44 -0400

    Get rid of pkg_resources.

--- glyph/const.py
+++ glyph/const.py
@@ -2,12 +2,13 @@
 
 import typing as T
 import os
-import pkg_resources
+from importlib import resources
 
 REGISTRY_CACHE_PATH: str = "~/.cache/glyph/registry.pckl"
 
-CONFIG_FILE_LOCATIONS: T.List[str] = [
-    os.path.expanduser("~/.glyph.yaml"),
-    os.path.expanduser("~/.glyph.yml"),
-    pkg_resources.resource_filename(__name__, "glyph.yaml"),
-]
+with resources.path("glyph", "glyph.yaml") as resource_path:
+    CONFIG_FILE_LOCATIONS: T.List[str] = [
+        os.path.expanduser("~/.glyph.yaml"),
+        os.path.expanduser("~/.glyph.yml"),
+        str(resource_path),
+    ]
